fb3eeae539bfe1b1f570f70c40b1556ff9dde54e
gumeeee <gaxco123@gmail.com>
2025-07-06 01:53:37 -0300

    docs: add documentation for create Opening

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -7,6 +7,18 @@ import (
 	"github.com/gumeeee/go-jobs-api/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Sumary Create Opening
+// @Description Create a new job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param request body CreateOpeningRequest true "Opening details"
+// @Success 200 {object} CreateOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [post]
 func CreateOpeningHandler(ctx *gin.Context) {
 	request := CreateOpeningRequest{}
 
